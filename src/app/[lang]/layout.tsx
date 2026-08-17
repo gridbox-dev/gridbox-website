50d8cb95caef0b3ab32e743d780bf7694444ecbb
José Tomás del Real Breque <tomas@gridbox.dev>
2026-08-16 20:55:46 -0400

    feat: add fonts variables on root layout

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -5,6 +5,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import type { JSX, PropsWithChildren } from 'react';
+import { fontSans, fontSerif } from '@/config/ui/fonts';
 import { DEFAULT_LOCALE } from '@/proxy';
 import type { StaticParams } from '@/types/common/next';
 
@@ -27,7 +28,10 @@ async function RootLayout(props: PropsWithChildren<StaticParams>): Promise<JSX.E
 	const { lang } = await params;
 
 	return (
-		<html lang={lang || DEFAULT_LOCALE}>
+		<html
+			lang={lang || DEFAULT_LOCALE}
+			className={`${fontSans.variable} ${fontSerif.variable}`}
+		>
 			<body>{children}</body>
 		</html>
 	);
