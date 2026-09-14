2461001c1356a3cefc885e7bb7dda3c7bbd523e9
José Tomás del Real Breque <tomas@gridbox.dev>
2026-09-14 00:19:25 -0300

    patch: add z-stacking priority to header container

--- src/modules/layout/header/components/header-container.tsx
+++ src/modules/layout/header/components/header-container.tsx
@@ -11,7 +11,7 @@ import { tv } from '@/config/ui/tw-variants';
 
 const styles = tv({
 	slots: {
-		base: 'flex flex-col items-center justify-center w-full bg-primary border-b border-tertiary',
+		base: 'relative z-50 flex flex-col items-center justify-center w-full bg-primary border-b border-tertiary',
 		inner: 'flex items-center justify-between h-fit w-full max-w-container pl-16 pr-8 tablet:pl-32 tablet:pr-24',
 	},
 });
